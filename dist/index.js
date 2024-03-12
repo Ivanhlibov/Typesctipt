5071441e0511516d1f3c2584cae2a0db25a668e2
Ivanhlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-03-12 22:53:35 +0200

    еуяе

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+// let user : string = 'Ivan'
+// let id: unknown
+//  id = 5;
+//  id = '5';
+const coords = [50.3423, 44, 456446];
+const rgb = [255, 0, 0];
+let union;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// let user : string = 'Ivan'
+// let id: unknown
+//  id = 5;
+//  id = '5';
+
+
+const coords: number[] = [50.3423, 44,456446]
+
+const rgb = [255, 0, 0]
+
+type numArr = {
+    number: number[]
+}
+
+let union : string | number
