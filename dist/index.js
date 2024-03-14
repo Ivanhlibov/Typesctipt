4a94b59a254f28d0fb73d030cf54fdea5c4ed7e1
Ivanhlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-03-15 00:01:41 +0200

    ыефке

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,44 @@
 // let id: unknown
 //  id = 5;
 //  id = '5';
-const coords = [50.3423, 44, 456446];
-const rgb = [255, 0, 0];
-let union;
+const UserName = 'Ivan';
+// type Arrnum = [number, number, number]
+// const coords: readonly Arrnum = [255.6767, 555.8789]
+// -----------------------------------------------------------------
+const tenps = [255, 0, 0];
+const userId = 5;
+const taskId = 'text';
+const notification = 'error';
+// ------------------------------------------------------------------
+var PizzaSize;
+(function (PizzaSize) {
+    PizzaSize["Small"] = "Small";
+    PizzaSize["Medium"] = "Medium";
+    PizzaSize["Large"] = "Lage";
+})(PizzaSize || (PizzaSize = {}));
+console.log(PizzaSize);
+PizzaSize.Medium;
+const order = {
+    size: PizzaSize.Large,
+    qoantity: 5
+};
+const config = {
+    selector: '#plugin-1',
+    perPage: 2,
+    statrtIndex: 0,
+    draggable: false
+};
+const employees = {
+    poly: 5,
+    kiwi: 10,
+    mango: 15,
+    ajax: 50
+};
+// -----------------------------------
+const fn = function (param1, param2) {
+    return param1 + param2;
+};
+fn(5, 5);
+console.log(fn(5, 5));
+// fn('5' , '5' ) error
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,88 @@
 //  id = '5';
 
 
-const coords: number[] = [50.3423, 44,456446]
+const UserName: string = 'Ivan'
 
-const rgb = [255, 0, 0]
+// type Arrnum = [number, number, number]
 
-type numArr = {
-    number: number[]
+// const coords: readonly Arrnum = [255.6767, 555.8789]
+
+// -----------------------------------------------------------------
+const tenps = [255, 0 ,0]
+
+type Id = number | string
+
+
+
+const userId: Id = 5
+const taskId: Id = 'text'
+
+// -----------------------------------------------------------------
+type Alerts = 'request' | 'success' | 'error';
+
+const notification: Alerts = 'error'
+// ------------------------------------------------------------------
+
+enum PizzaSize {
+    Small = 'Small',
+    Medium = 'Medium' ,
+    Large = 'Lage'
 }
 
-let union : string | number
+
+console.log(PizzaSize);
+PizzaSize.Medium
+
+
+const order = {
+    size: PizzaSize.Large,
+    qoantity: 5
+}
+
+// ----------------------------
+interface PlagConfig{
+  readonly  selector: string,
+    perPage: number,
+    statrtIndex: number, 
+    draggable: boolean
+}
+
+
+const config= {
+    selector: '#plugin-1',
+    perPage: 2,
+    statrtIndex: 0,
+    draggable: false
+    }
+
+    interface Money {
+        [employees: string]: | number
+    }
+
+    const employees = {
+    poly: 5,
+    kiwi: 10,
+    mango: 15,
+    ajax: 50
+    }
+
+
+
+
+
+
+
+
+
+
+    // -----------------------------------
+
+    const fn = function (param1: number, param2: number ) {
+        return param1 + param2
+        
+    }
+
+     fn( 5, 5)
+    console.log(fn(5, 5));
+    
+    // fn('5' , '5' ) error
